b907716f73a6cd46a4fbbb0dd211b6a479b571c3
BlackWasp <kylian.cresci1@gmail.com>
2021-12-06 17:03:23 +0100

    update

--- FaceResearch.py
+++ FaceResearch.py
@@ -1,12 +1,12 @@
-from cv2 import *
+import cv2
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # On lit test.jpg
-img = imread("images_test\\test.jpg")
+img = cv2.imread("images_test\\test.jpg")
 
 # changement vers un format non RGB
-grayScaled_img = cvtColor(img, cv2.COLOR_RGB2GRAY)
+grayScaled_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
 
 # On récupère les coordonnées (x y w h) du visage sur la photo
@@ -21,12 +21,12 @@ for (x, y, w, h) in face_coordinates:
     # param_3 = un tuple des coordonnées en bas à droit
     # param_4 = un tuple de couleur BGR
     # param_5 = L'épaisseur du rectangle
-    rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
     
 
 # Affiche une image
-imshow("test", img)     # affichage de l'image originale
+cv2.imshow("test", img)     # affichage de l'image originale
 # imshow("test", grayScaled_img)     # affichage de l'image grisé
 
 # Attend la pression d'une touche
-waitKey()
+cv2.waitKey()
--- webcamResearch.py
+++ webcamResearch.py
@@ -1,31 +1,24 @@
-from cv2 import *
+import cv2
 
 
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # driver de la caméra par défaut de l'ordinateur
-webCam = VideoCapture(0)
+webCam = cv2.VideoCapture(0)
 
 
 while True:
     frame_read, frame = webCam.read()
-
     # changement vers un format non RGB
-    grayScaled_img = cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
+    grayScaled_img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     # # On récupère les coordonnées (x y w h) du visage sur la photo
     face_coordinates = trained_face_data.detectMultiScale(grayScaled_img)
-
     for (x, y, w, h) in face_coordinates:
-        rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
-
-
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 8)
     # Affiche une image
-    imshow("test", frame)     # affichage de l'image originale
-
+    cv2.imshow("test", frame)     # affichage de l'image originale
     # Attend la pression d'une touche
-    key = waitKey(1)
-
+    key = cv2.waitKey(1)
     if key == 27:
         break
 
